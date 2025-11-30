99dfc57aa6275cff0b2b9dd79378734a0c8b9c8b
Pitiș Radu <pitis.radu@protonmail.com>
2025-11-30 22:27:35 +0200

    feat: react query provider

--- applications/next-boilerplate-client/src/app/layout.tsx
+++ applications/next-boilerplate-client/src/app/layout.tsx
@@ -1,15 +1,9 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import ApolloWrapper from '../lib/ApolloWrapper'
+import { Providers } from './components/Providers'
 
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Eversports frontend assignment',
-  description: 'Looking forward to see what you come up with!',
-}
 
 export default function RootLayout({
   children,
@@ -19,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ApolloWrapper>{children}</ApolloWrapper>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
